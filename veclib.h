b36b2d96b6db23c96dc8c6d112563fa7f1b1bbcc
rahul-a5 <myrahulanand20@gmail.com>
2020-10-15 23:01:32 +0530

    Basic Implementation

---
+++ veclib.h
@@ -0,0 +1,36 @@
+
+#ifndef veclib
+#define veclib
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define DIM 4
+// vector type
+typedef float vector[DIM];
+
+// functions
+// print vector
+void print(float vec_a);
+
+// vector addition
+float* add(vector vec_a, vector vec_b);
+
+// elementwise product
+float* eleProd(vector vec_a, vector vec_b);
+
+// difference of two vectors
+float* diff(vector vec_a, vector vec_b);
+
+// dot product
+float dotprod(vector vec_a, vector vec_b);
+
+//angle between 2 vector
+float angle(vector vec_a, vector vec_b);
+
+//norm of 2 vector
+float norm(vector vec_a, vector vec_b);
+
+#endif
---
+++ vecmain.c
@@ -0,0 +1,20 @@
+#include "veclib.h"
+
+
+int main() {
+	vector vecA, vecB, vecC;
+
+	printf("Please enter first vector\n");
+    for(int i=0;i<DIM;i++){
+        scanf("%f",&vecA[i]);
+    }
+
+    printf("Please enter second vector\n");
+    for(int i=0;i<DIM;i++){
+        scanf("%f",&vecB[i]);
+    }
+
+
+
+    return 0;
+} 
