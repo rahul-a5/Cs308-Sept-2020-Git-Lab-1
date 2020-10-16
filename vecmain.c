e142f3c0e6edbf2860e982f417af915a57604bc1
Dilshaad-B18041 <dilshaadmuthalif@ymail.com>
2020-10-16 12:48:25 +0530

    Added function to return angle between two vectors
    Updated vecmain.c and created vecangle.c

--- vecmain.c
+++ vecmain.c
@@ -1,34 +1,39 @@
 #include "veclib.h"
 #include <stdio.h>
 
+int main()
+{
+    vector vecA, vecB, vecC;
 
-
-int main() {
-	vector vecA, vecB, vecC;
-
-	printf("Please enter first vector\n");
-    for(int i=0;i<DIM;i++){
-        scanf("%f",&vecA[i]);
+    printf("Please enter first vector\n");
+    for (int i = 0; i < DIM; i++)
+    {
+        scanf("%f", &vecA[i]);
     }
 
     printf("Please enter second vector\n");
-    for(int i=0;i<DIM;i++){
-        scanf("%f",&vecB[i]);
+    for (int i = 0; i < DIM; i++)
+    {
+        scanf("%f", &vecB[i]);
     }
-    float * added_vec;
-    added_vec=add(vecA,vecB);
+    float *added_vec;
+    added_vec = add(vecA, vecB);
     printf("Resultant vector after addition\n");
     print(added_vec);
 
-    float * EleProd_vec;
-    EleProd_vec=eleProd(vecA,vecB);
+    float *EleProd_vec;
+    EleProd_vec = eleProd(vecA, vecB);
     printf("Resultant vector after element-wise Product\n");
     print(EleProd_vec);
 
-    float * Diff_vec;
-    Diff_vec=diff(vecA,vecB);
+    float *Diff_vec;
+    Diff_vec = diff(vecA, vecB);
     printf("Resultant difference Vector:\n"); // vec1 - vec2
     print(Diff_vec);
 
+    float Angle_vec;
+    Angle_vec = angle(vecA, vecB);
+    printf("Angle between the vectors: %f\n", Angle_vec);
+
     return 0;
-} 
+}
